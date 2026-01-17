67b8bc0ff13c73171d8e139565a144a521c9b97b
IlliaAfanasenco <ilia.biz2@gmail.com>
2026-01-17 16:09:54 +0100

    configured the routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import MainLayouts from "./layouts/MainLayouts.tsx";
+import Home from "./pages/Home/Home.tsx";
+import About from "./pages/About/About.tsx";
+import Gallery from "./pages/Gallery/Gallery.tsx";
 
 
 const App = () => {
     return (
-        <div>
-
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<MainLayouts/>}>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
+                    <Route path="/gallery" element={<Gallery/>}/>
+                </Route>
+                <Route path="*" element={<div className="container section">404</div>}/>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
